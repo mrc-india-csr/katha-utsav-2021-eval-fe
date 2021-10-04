a83fbbacd3ea45c4ae248f7ffa40b0b195b506db
Hariananthan K P <hariananthan93@gmail.com>
2021-10-04 16:25:04 +0530

    Updated Login Styles

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,10 +1,61 @@
 import React, {useState} from 'react';
 import '../styles/global.scss';
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Typography  from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import TextField from '@material-ui/core/TextField';
 import axios from "axios";
+import logo from '../client/assets/logo.png';
 
+const useStyles = makeStyles(theme => ({
+  body:{
+    backgroundColor: "#FFF5CA",
+    position: "absolute",
+    width: "99%",
+    height:"98%"   
+  },
+  loginForm:{
+    margin:"10% 30% 10% 30%",
+    width: "500px",
+    height: "200px",
+    position:"relative",
+    padding:"5vw"
+  },
+  loginButton:{
+    color:"white",
+    backgroundColor:"#98248D",
+    width:"30vw",
+    marginLeft: "25px",
+    textTransform: "none",
+    "&:hover": {
+      backgroundColor: "#9D4395"
+  },
+  },
+  textfield:{
+    width:"30vw",
+    marginLeft: "25px"
+  },
+  logo:{
+    marginRight:'25px',
+    width: '50px',
+    height: '50px',
+    display:"inline"
+  },
+  title:{
+    fontFamily: 'Poppins',
+    fontSize: '30px',
+    lineHeight: '45px',
+    fontWeight: '400',
+    letterSpacing: '0.01em',
+    display:"inline"
+  },
+})
+)
 const Auth = () => {
   const [email, setEmail] = useState('');
-
+  const classes = useStyles();
   const loginSubmit = async (event) => {
     event.preventDefault();
     await axios.post('/api/login', {email}).then((res) => {
@@ -15,21 +66,32 @@ const Auth = () => {
   }
 
   return (
-    <form onSubmit={loginSubmit}>
-      <div>
-        <label htmlFor='email-input' defaultValue='Email'>Email: </label>
-        <input
-          name='email' onChange={(event) => {
+    <div className={classes.body}>
+      <Paper className={classes.loginForm}>       
+      <Grid container>
+        <Grid item>
+      <img src={logo} className={classes.logo} /> 
+      </Grid>
+      <Grid item>
+          <Typography className={classes.title}> Katha Utsav Evaluation Portal </Typography>
+      </Grid>
+      </Grid>
+          <br/>
+        <Typography align="center">Enter your mail ID to login</Typography>  
+        <br/>
+        <TextField 
+        id="email" 
+        variant="outlined" 
+        label="Email ID" 
+        className={classes.textfield}
+        onChange={(event) => {
           setEmail(event.target.value)
-        }} type='email'
-          value={email}
+        }}
         />
-      </div>
-      <br/>
-      <div>
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+        <br/><br/>
+        <Button className={classes.loginButton} onClick={loginSubmit}>Login</Button>
+      </Paper>
+    </div>
   );
 }
 
