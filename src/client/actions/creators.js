a13b144e7a4c0a8b4e686fd46db209c8f263fa40
Nizarahammed A <nizarahammed14@gmail.com>
2021-10-05 12:47:17 +0530

    remove example action creator

--- src/client/actions/creators.js
+++ src/client/actions/creators.js
@@ -4,10 +4,6 @@ import { SET_PENDING_STATUS_COUNT,
   SET_STATUS_COUNT
  } from "./types";
 
-export const getExample =  data => {
-  return { type: EXAMPLE, data };
-};
-
 export const setPendingStatusCount = data => {
   return { type: SET_PENDING_STATUS_COUNT, data };
 }
