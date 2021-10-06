eaa2f292781ba55308aeb04a8878151005d35ced
HemanthaRajan <Hemantharajan.V1@mrcooper.com>
2021-10-06 13:31:25 +0530

    Updated download url

--- src/routes/middleware/updateDownloadUrl.js
+++ src/routes/middleware/updateDownloadUrl.js
@@ -4,7 +4,7 @@ export const UpdateDownloadUrl = (req, res, next) => {
   try {
     const { s3Folder, s3BucketRegion, s3BucketName } = config
     res.locals.studentDetails = res.locals.studentDetails.map(data => {
-      const filename = data.file_location_url.substring(data.file_location_url.split('/', 6).join('/').length);
+      const filename = data.file_location_url.substring((data.file_location_url.split('/', 6).join('/').length)+1);
       return {
         ...data,
         file_location_url: `https://${s3BucketName}.s3.${s3BucketRegion}.amazonaws.com/${s3Folder + filename}`
