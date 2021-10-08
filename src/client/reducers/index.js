a4f86ad003eeba1e6c652a9a3067e99251a2dc3c
HemanthaRajan <Hemantharajan.V1@mrcooper.com>
2021-10-08 17:02:41 +0530

    confirmation modal

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import statusCount from './status-count-reducer';
 import studentDetails from './student-details-reducer';
-
+import displayModalReducer from './display-modal-reducer';
 export const rootReducerObject = {
     statusCount,
-    studentDetails
+    studentDetails,
+    displayModalReducer
 };
 
 export const rootReducer = combineReducers(rootReducerObject);
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import '../styles/global.scss'
 import '../styles/dashboard.scss'
 import axios from "axios";
 import logo from '../client/assets/logo.png';
 import logoutIcon from '../client/assets/logout.png';
 import StatusTile from '../components/entrypoints/statusTile';
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const Dashboard = (props) => {
   const logoutClick = () => {
-    console.log('logout called');
     axios.get('/api/logout').then((res) => {
       window.location = res.data.redirect;
     }).catch((e) => {
@@ -19,15 +21,25 @@ const Dashboard = (props) => {
     <div className="dashboard">
       <div className="appBar">
         <div>
-          <img src={logo} className="logo" /> 
-          <span className="title">Katha Utsav Evaluation Portal</span>    
+          <img src={logo} className="logo" />
+          <span className="title">Katha Utsav Evaluation Portal</span>
         </div>
           <button className="logout" onClick={logoutClick}>
-          <img src={logoutIcon} className="logoutIcon" /> 
+          <img src={logoutIcon} className="logoutIcon" />
             Logout
           </button>
       </div>
       <StatusTile/>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover/>
     </div>
   )
 };
